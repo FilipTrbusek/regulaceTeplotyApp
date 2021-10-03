5a97a9cde8715df01583483acdf9d841eba0bc2e
Filip Trbusek <filiptrbusek@gmail.com>
2021-10-03 16:46:47 +0200

    regulace teploty cviceni completed

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+let teplota = 23;
+
+if (teplota <= 10) {
+  console.log("Je chladno.");
+} else if (teplota > 11 && teplota < 26) {
+  console.log("Je teplo.");
+} else {
+  console.log("Je horko.");
+}
